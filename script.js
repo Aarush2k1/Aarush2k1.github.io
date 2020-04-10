e1364dff449cb7fdbf2c8249b209f9393805f737
Aarush2k1 <56411093+Aarush2k1@users.noreply.github.com>
2020-04-10 23:43:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,112 @@
+const feed = document.querySelector(".open-button");
+feed.addEventListener("click", () => {
+  openForm();
+});
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+function myFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+//Carousel
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+ //Animate
+ function makeEaseInOut(timing) {
+  return function(timeFraction) {
+    if (timeFraction < .5)
+      return timing(2 * timeFraction) / 2;
+    else
+      return (2 - timing(2 * (1 - timeFraction))) / 2;
+  }
+}
+function bounce(timeFraction) {
+  for (let a = 0, b = 1, result; 1; a += b, b /= 2) {
+    if (timeFraction >= (7 - 4 * a) / 11) {
+      return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+    }
+  }
+}
+
+let bounceEaseInOut = makeEaseInOut(bounce);
+
+brick.onclick = function() {
+  animate({
+    duration: 3000,
+    timing: bounceEaseInOut,
+    draw: function(progress) {
+      brick.style.left = progress * 500 + 'px';
+    }
+  });
+};
+
+
+
+
+
+//alert("By clicking ok you agree Terms and conditions");
+
+
+function changeText(id) {
+ 
+  id.innerHTML = "Ooops!";
+  id.addEventListener("click",()=>{
+    recoverText(id);
+  })
+    
+}
+function recoverText(i){
+  i.innerHTML="Programs";
+}
+function mOver(obj) {
+  obj.innerHTML = "Thank You";
+  document.getElementById(".1").style.display="block";
+}
+function mOut(obj) {
+ obj.innerHTML = "School";
+ }
